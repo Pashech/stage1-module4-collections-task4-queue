489e3adc26cc10ec4f3e05b9375ad9817dbc7825
Pavel Setsko <Pashech555@gmail.com>
2022-09-11 20:46:49 +0300

    done

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,23 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < everyDishNumberToEat - 1; i++) {
+                queue.add(queue.poll());
+            }
+            list.add(queue.poll());
+        }
+
+        System.out.println(list);
+        return list;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,20 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> result = new PriorityQueue<>(comparator);
+        result.addAll(firstList);
+        result.addAll(secondList);
+
+        return result;
     }
+
+    private Comparator<String> comparator = new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            return o2.compareTo(o1);
+        }
+    };
 }
